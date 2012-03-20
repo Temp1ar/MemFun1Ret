7a40228682a3690d988bd94fef4e05c8b3422768
Alexey Korovin <alexey.korovin@spbau.se>
2012-03-20 16:40:15 +0400

    First and probably last commit.

---
+++ memfun_ret/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+template<class T, class R, class A>
+struct Functor {
+	typedef R (T::*method_ref)(A);
+	method_ref ref_;
+
+	Functor(method_ref ref) : ref_(ref) {}
+
+	R operator()(T& t, A a) {
+		return (t.*ref_)(a);
+	}
+};
+
+template<class T, class R, class A>
+struct FunctorConst {
+	typedef R (T::*method_ref)(A) const;
+	method_ref ref_;
+
+	FunctorConst(method_ref ref) : ref_(ref) {}
+
+	R operator()(T& t, A a) {
+		return (t.*ref_)(a);
+	}
+};
+
+template<class T, class R, class A>
+Functor<T, R, A> mem_fun1_ret(R (T::*f)(A)) {
+	return Functor<T, R, A>(f);
+}
+
+template<class T, class R, class A>
+FunctorConst<T, R, A> mem_fun1_ret(R (T::*f)(A) const) {
+	return FunctorConst<T, R, A>(f);
+}
+
+struct Test {
+	int test(int) const {
+		return 1;
+	}
+};
+
+int main() {
+	std::vector<int> v;
+	v.push_back(2);
+
+	auto f = mem_fun1_ret(&std::vector<int>::reserve);
+	f(v, 10);
+	std::cout << v.capacity() << std::endl;
+	
+	auto fc = mem_fun1_ret(&Test::test);
+	Test a;
+	std::cout << fc(a, 1) << std::endl;
+
+	return 0;
+}
